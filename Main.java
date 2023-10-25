1264c81963115512a9d4e4b3bd5f4e4cdd80adb4
Mihai <115227631+MeHg1h@users.noreply.github.com>
2023-10-25 21:58:53 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
-public class Main {
+public class main {
     public static void main(String[] args) {
         int[] month_days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         String[] months = {"January", "February", "March", "April", "May", "June",
                 "July", "August", "September", "October", "November", "December"
         };
+        int totalDays = 0;
         for (int i = 0; i < months.length; i++) {
-            String abbreviatedMonth = months[i].substring(0, 3);
-            System.out.println(abbreviatedMonth + " has " + month_days[i] + " days.");
+            if (month_days[i] < 31) {
+                totalDays += month_days[i];
+            }
         }
+        System.out.println("Total number of days for months with fewer than 31 days: " + totalDays);
     }
 }
